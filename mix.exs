47570b5b912446d9836605db69eada9fb45f1cfe
Laszlo Korte <email@laszlokorte.de>
2026-02-26 17:31:35 +0100

    recalculate color map

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KinoZoetrope.MixProject do
   def project do
     [
       app: :kino_zoetrope,
-      version: "0.10.0",
+      version: "0.11.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
